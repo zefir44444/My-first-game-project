18caea764f0fcada03e59a91a66ab9c2ceba646c
Denis <denisfateev@gmail.com>
2024-03-03 19:05:01 +0200

    Add new update with controller

--- Assets/Code/Sneak_Control.cs
+++ Assets/Code/Sneak_Control.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class Sneak_Control : MonoBehaviour
 {
+    public float speed = 1;
 
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Start is run");
     }
 
     // Update is called once per frame
     void Update()
     {
         //Read input from the player
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        
-        Debug.Log($"Horizontal: {horizontal}, Vertical: {vertical}");
+        Vector2 input = ReadInput();
+
+        //Read postition of the snake
+        Vector3 movemenet = new Vector3(input.x, input.y, 0) * speed * Time.deltaTime;
+        transform.position += movemenet;
+    }
+
+    private static Vector2 ReadInput()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        return new Vector2(horizontal, vertical);
     }
 }
